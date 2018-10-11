b758140fb855a8556beb1f911b4c08175f26c136
cgiohidalgo <cgiohidalgo@gmail.com>
2018-10-11 11:25:22 -0500

    moviendo archivos

--- docentes.js
+++
@@ -1,9 +0,0 @@
-db.users.insert({"username" : "joshuadtm", "realname" : "Docente Joshua", "language" : "en", "password" : "7c6707711f854ec5e85b67accf55a2c6201fd00d08ad66cecb0790570cbda673a324480d66d14f113b24ae6952bd13ac8b22b310f0ae9e43fe690ac4e1362c3c", "email" : "joshuadtm@gmail.com", "bindings" : {  } });
-db.users.insert({"username" : "leidy.carvajal.o", "realname" : "Docente Leidy ", "language" : "en", "password" : "6f4e549548227d050b780b705909a23055b0b11cd0106cba08ac28c22990b27599ccb1e96df6869123716d4d091ae1d9e817c245f885c91c4147e8131367ae82", "email" : "leidy.carvajal.o@correounivalle.edu.co", "bindings" : {  } });
-db.users.insert({"username" : "yesid.ospitia", "realname" : "Docente Yesid", "language" : "en", "password" : "3acca5d412314789a9000ff78ebcc0411e98c151d11777661d7d0bc3ff4db4450808c0559ace7a951a9def3b790587147038638145cb058d3ef5a46cedf423a4", "email" : "yesid.ospitia@correounivalle.edu.co", "bindings" : {  } });
-
-
- 
-
-
-
